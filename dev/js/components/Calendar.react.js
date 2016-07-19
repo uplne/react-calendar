b09ffdcff49c81542a4120389c6b12b5faa63a87
Steve Vadocz <uplneinak@gmail.com>
2016-07-19 13:35:34 +0100

    New route of the calendar

--- dev/js/components/Calendar.react.js
+++ dev/js/components/Calendar.react.js
@@ -36,50 +36,46 @@ export default class Calendar extends React.Component {
     }
 
     render () {
-        if (this.props.isCalendarOpen) {
-            const startOfTheMonth = getStartOfTheMonth(this.state.date);
-            const monthName = getMonthName(this.state.date);
-            const weekDayNames = getWeekDayNames();
-            const weeks = getWeeks(startOfTheMonth);
+        const startOfTheMonth = getStartOfTheMonth(this.state.date);
+        const monthName = getMonthName(this.state.date);
+        const weekDayNames = getWeekDayNames();
+        const weeks = getWeeks(startOfTheMonth);
 
-            return (
-                <div className="datepicker box box--shadow">
-                    <table className="datepicker__table">
-                        <thead className="datepicker__header">
-                            <tr>
-                                <td colSpan="5"><span>{monthName}</span></td>
-                                <td>
-                                    <ArrowBtn classes="btn btn--clean btn--icon datepicker__arrow datepicker__arrow--left" onClick={this.decreaseMonth}>
-                                        <svg viewBox="0 0 20 20"><path d="M12.452 4.516c.446.436.48 1.043 0 1.576L8.705 10l3.747 3.908c.48.533.446 1.14 0 1.574-.445.436-1.197.408-1.615 0-.418-.406-4.502-4.695-4.502-4.695C6.112 10.57 6 10.285 6 10s.112-.57.335-.79c0 0 4.084-4.286 4.502-4.694s1.17-.436 1.615 0z"/></svg>
-                                    </ArrowBtn>
-                                </td>
-                                <td>
-                                    <ArrowBtn classes="btn btn--clean btn--icon datepicker__arrow datepicker__arrow--right"  onClick={this.increaseMonth}>
-                                        <svg viewBox="0 0 20 20"><path d="M12.452 4.516c.446.436.48 1.043 0 1.576L8.705 10l3.747 3.908c.48.533.446 1.14 0 1.574-.445.436-1.197.408-1.615 0-.418-.406-4.502-4.695-4.502-4.695C6.112 10.57 6 10.285 6 10s.112-.57.335-.79c0 0 4.084-4.286 4.502-4.694s1.17-.436 1.615 0z"/></svg>
-                                    </ArrowBtn>
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody className="datepicker__week">
-                            <tr>
-                                { weekDayNames.map((day, i) => {
-                                    return (
-                                        <td key={i}><b className="datepicker__day">{day}</b></td>
-                                    );
-                                })}
-                            </tr>
-                            { weeks.map((week, i) => {
+        return (
+            <div className="datepicker box box--shadow">
+                <table className="datepicker__table">
+                    <thead className="datepicker__header">
+                        <tr>
+                            <td colSpan="5"><span>{monthName}</span></td>
+                            <td>
+                                <ArrowBtn classes="btn btn--clean btn--icon datepicker__arrow datepicker__arrow--left" onClick={this.decreaseMonth}>
+                                    <svg viewBox="0 0 20 20"><path d="M12.452 4.516c.446.436.48 1.043 0 1.576L8.705 10l3.747 3.908c.48.533.446 1.14 0 1.574-.445.436-1.197.408-1.615 0-.418-.406-4.502-4.695-4.502-4.695C6.112 10.57 6 10.285 6 10s.112-.57.335-.79c0 0 4.084-4.286 4.502-4.694s1.17-.436 1.615 0z"/></svg>
+                                </ArrowBtn>
+                            </td>
+                            <td>
+                                <ArrowBtn classes="btn btn--clean btn--icon datepicker__arrow datepicker__arrow--right"  onClick={this.increaseMonth}>
+                                    <svg viewBox="0 0 20 20"><path d="M12.452 4.516c.446.436.48 1.043 0 1.576L8.705 10l3.747 3.908c.48.533.446 1.14 0 1.574-.445.436-1.197.408-1.615 0-.418-.406-4.502-4.695-4.502-4.695C6.112 10.57 6 10.285 6 10s.112-.57.335-.79c0 0 4.084-4.286 4.502-4.694s1.17-.436 1.615 0z"/></svg>
+                                </ArrowBtn>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody className="datepicker__week">
+                        <tr>
+                            { weekDayNames.map((day, i) => {
                                 return (
-                                    <Week key={i} day={week} shownDate={this.state.date} onClick={this.props.onClick}/>
+                                    <td key={i}><b className="datepicker__day">{day}</b></td>
                                 );
                             })}
-                        </tbody>
-                    </table>
-                </div>
-            );
-        } else {
-            return false;
-        }
+                        </tr>
+                        { weeks.map((week, i) => {
+                            return (
+                                <Week key={i} day={week} shownDate={this.state.date} onClick={this.props.onClick}/>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        );
     }
 }
 
--- dev/js/components/TimeWizardBox.react.js
+++ dev/js/components/TimeWizardBox.react.js
@@ -1,57 +1,124 @@
 import React, {PropTypes} from 'react';
+import moment         from 'moment';
 
-import TimeWizardOptionList from './TimeWizardOptionList.react';
-import DateTimePicker from './DateTimePicker.react';
+import IconTextField  from './IconTextField.react';
+import {TimeValidate, TimeFormat} from '../utils/TimeValidate';
+import Calendar       from './Calendar.react';
 
 export default class TimeWizardBox extends React.Component {
 	constructor() {
 		super();
 
 		this.state = {
-			isOptionListOpen: false,
-			isOneDayPickerOpen: false
-		};
+            isCalendarOpen: false,
+            dateStart: '',
+            timeStart: '',
+            dateEnd: '',
+            timeEnd: ''
+        };
 
-		this.openOneDayPicker = this.openOneDayPicker.bind(this);
-		this.openDayRangePicker = this.openDayRangePicker.bind(this);
 		this.onSave = this.onSave.bind(this);
+		this.handleTimeChange = this.handleTimeChange.bind(this);
+        this.openCalendar = this.openCalendar.bind(this);
+        this.onCalendarClick = this.onCalendarClick.bind(this);
 	}
 
-	componentDidMount() {
-		this.setState({
-			isOptionListOpen: true
-		});	
+	onSave() {
+		this.props.submitWizard(this.state);
 	}
 
-	openOneDayPicker() {
-		this.setState({
-			isOptionListOpen: false,
-			isOneDayPickerOpen: true
-		});
-	}
+	openCalendar(e) {
+        this.setState({
+            isCalendarOpen: true,
+            calendarRef: e.target.id
+        });
+    }
 
-	openDayRangePicker() {
+    handleTimeChange(id, value) {
+        this.setState({
+            [id]: value,
+        });
+    }
 
-	}
+    onCalendarClick(day) {
+        this.setState({
+            isCalendarOpen: false,
+            [this.state.calendarRef]: moment(day).format('YYYY-MM-DD'), // Convert to ISO8601 format,
+            calendarRef: ''
+        });
+    }
 
-	onSave(data) {
-		this.setState({
-			isOptionListOpen: true,
-			isOneDayPickerOpen: false
-		});
-		this.props.submitWizard(data);
-	}
+    getRightMoment(type) {
+        if (this.state[type] === '') {
+            return '';
+        }
+
+        return TimeFormat(this.state[type]);
+    }
+
+    renderCalendar() {
+    	if (this.state.isCalendarOpen) {
+	    	return (
+	    		<Calendar
+		            dateStart={moment()}
+		            onClick={this.onCalendarClick} />
+		    );
+		} else {
+			return false;
+		}
+    }
 
     render () {
         return (
-            <div className="time-wizard box--shadow" onMouseDown={this.props.onMouseDown} onMouseUp={this.props.onMouseUp}>
-            	<TimeWizardOptionList 
-            		isOptionListOpen={this.state.isOptionListOpen}
-            		openOneDayPicker={this.openOneDayPicker}
-            		openDayRangePicker={this.openDayRangePicker} />
-                <DateTimePicker 
-                    onSave={this.onSave}
-                    isOneDayPickerOpen={this.state.isOneDayPickerOpen} />
+            <div className="time-wizard box--shadow">
+            	<div className="date-time-picker box--small" onMouseDown={this.props.onMouseDown} onMouseUp={this.props.onMouseUp}>
+                    <h4>Start date and time</h4>
+                    <div className="date-time-picker__input-box">
+                        <IconTextField
+                            id="dateStart"
+                            name="dateStart"
+                            classNames="input input--clean"
+                            value={this.getRightMoment('dateStart')}
+                            onFocus={this.openCalendar}>
+                            <svg className="icontextfield__icon" viewBox="0 0 20 20"><path d="M17 3h-1v2h-3V3H7v2H4V3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 14H3V9h14v8zM6.5 1h-2v3.5h2V1zm9 0h-2v3.5h2V1z"/></svg>
+                        </IconTextField>
+                        <IconTextField
+                            id="timeStart"
+                            name="timeStart"
+                            classNames="input input--clean"
+                            value={this.state.timeStart}
+                            valid={this.state.isTimeValid}
+                            onChange={this.handleTimeChange}>
+                            <svg className="icontextfield__icon" viewBox="0 0 32 32"><path d="M20.586 23.414L14 16.828V8h4v7.172l5.414 5.414zM16 0C7.163 0 0 7.163 0 16s7.163 16 16 16 16-7.163 16-16S24.837 0 16 0zm0 28C9.373 28 4 22.627 4 16S9.373 4 16 4s12 5.373 12 12-5.373 12-12 12z"/></svg>
+                        </IconTextField>
+                    </div>
+                </div>
+                <div className="date-time-picker box--small" onMouseDown={this.props.onMouseDown} onMouseUp={this.props.onMouseUp}>
+                    <h4>End date and time (optional)</h4>
+                    <div className="date-time-picker__input-box">
+                        <IconTextField
+                            id="dateEnd"
+                            name="dateEnd"
+                            classNames="input input--clean"
+                            value={this.getRightMoment('dateEnd')}
+                            onFocus={this.openCalendar}>
+                            <svg className="icontextfield__icon" viewBox="0 0 20 20"><path d="M17 3h-1v2h-3V3H7v2H4V3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 14H3V9h14v8zM6.5 1h-2v3.5h2V1zm9 0h-2v3.5h2V1z"/></svg>
+                        </IconTextField>
+                        <IconTextField
+                            id="timeEnd"
+                            name="timeEnd"
+                            classNames="input input--clean"
+                            value={this.state.timeEnd}
+                            valid={this.state.isTimeValid}
+                            onChange={this.handleTimeChange}>
+                            <svg className="icontextfield__icon" viewBox="0 0 32 32"><path d="M20.586 23.414L14 16.828V8h4v7.172l5.414 5.414zM16 0C7.163 0 0 7.163 0 16s7.163 16 16 16 16-7.163 16-16S24.837 0 16 0zm0 28C9.373 28 4 22.627 4 16S9.373 4 16 4s12 5.373 12 12-5.373 12-12 12z"/></svg>
+                        </IconTextField>
+                    </div>
+                </div>
+                <button
+                    className="btn btn--small btn--primary date-time-picker__btn"
+                    onClick={this.onSave}>Save</button>
+                {this.renderCalendar()}
             </div>
         );
     }
